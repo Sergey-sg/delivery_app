a1c09b7847f487e2bf79abf132b71737c3037431
Sergey Glushak <glushak_sergey@i.ua>
2022-07-12 22:16:53 +0300

    added settings for heroku

--- delivery_app/settings/settings.py
+++ delivery_app/settings/settings.py
@@ -40,6 +40,11 @@ DATABASES = {
     }
 }
 
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
@@ -67,6 +72,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
