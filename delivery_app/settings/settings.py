45e4490b99af1b45bb2ba156570a97561189d92a
Sergey Glushak <glushak_sergey@i.ua>
2022-07-13 14:12:54 +0300

    fixed ALLOWED_HOST for heroku

--- delivery_app/settings/settings.py
+++ delivery_app/settings/settings.py
@@ -57,7 +57,7 @@ db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
 
-ALLOWED_HOSTS = ['https://glacial-bayou-95274.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['glacial-bayou-95274.herokuapp.com', '127.0.0.1']
 
 # Application definition
 
